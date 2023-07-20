bf0f627728fc178679ee67933cd64e80cad1b98f
sarahst <sarah@stirlingbrandworks.com>
2023-07-20 17:18:59 -0400

    Development of the custom header

---
+++ partials/menu-overlay.php
@@ -0,0 +1,7 @@
+<section id="menu-overlay" class="h-100 w-100 fixed top-0 left-0 z-10 js-menu-overlay">
+    <div class="container">
+        <div class="py-8">
+            Menu
+        </div>
+    </div>
+</section>
